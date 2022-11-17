8ee3a7b1aba78f093ada6cf0b0640e2266b8272e
Héctor Francés <32682445+hfrances@users.noreply.github.com>
2022-11-17 12:50:53 +0100

    feat: add unit-tests for net451.

--- qckdev.Net.Http.Test.Common/Helpers.cs
+++ qckdev.Net.Http.Test.Common/Helpers.cs
@@ -32,27 +32,35 @@ namespace qckdev.Net.Http.Test.Common
 
 #if NET461_OR_GREATER || NETCOREAPP
 
-        var builder = new ConfigurationBuilder()
-                .AddJsonFile($"appsettings.json", true, true)
-                .AddJsonFile($"appsettings.{environment}.json", true, true)
-                .AddEnvironmentVariables();
+            var builder = new ConfigurationBuilder()
+                    .AddJsonFile($"appsettings.json", true, true)
+                    .AddJsonFile($"appsettings.{environment}.json", true, true)
+                    .AddEnvironmentVariables();
 
-        var config = builder.Build();
+            var config = builder.Build();
 
-        return config.Get<Configuration.Settings>();
-
+            return config.Get<Configuration.Settings>();
 #else
-            string fileName = "appsettings.json";
-            string fileNameByEnv = $"appsettings.{environment}.json";
-            var finalName
-                = (string.IsNullOrEmpty(environment?.Trim()) || !System.IO.File.Exists(fileNameByEnv)) ?
-                    fileName : fileNameByEnv;
 
-            using (var reader = new System.IO.StreamReader(finalName))
+            var fileName = "appsettings.json";
+            var fileNameByEnv = $"appsettings.{environment}.json";
+            var settings = new Configuration.Settings();
+
+            if (System.IO.File.Exists(fileName))
             {
-                return Newtonsoft.Json.JsonConvert.DeserializeObject<Configuration.Settings>(reader.ReadToEnd());
+                using (var reader = new System.IO.StreamReader(fileName))
+                {
+                    Newtonsoft.Json.JsonConvert.PopulateObject(reader.ReadToEnd(), settings);
+                }
             }
-
+            if (!string.IsNullOrEmpty(environment?.Trim()) && System.IO.File.Exists(fileNameByEnv))
+            {
+                using (var reader = new System.IO.StreamReader(fileNameByEnv))
+                {
+                    Newtonsoft.Json.JsonConvert.PopulateObject(reader.ReadToEnd(), settings);
+                }
+            }
+            return settings;
 #endif
         }
 
