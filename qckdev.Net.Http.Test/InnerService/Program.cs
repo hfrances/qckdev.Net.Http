c267333cc54dbae76a7b0d2f597ff513ee250f07
Héctor Francés <32682445+hfrances@users.noreply.github.com>
2023-03-29 12:47:33 +0200

    Problem with Newtonsoft version

--- qckdev.Net.Http.Test/InnerService/Program.cs
+++ qckdev.Net.Http.Test/InnerService/Program.cs
@@ -1,39 +1,39 @@
 ﻿#if NET461_OR_GREATER || NETCOREAPP
-using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+//using Microsoft.Extensions.Hosting;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.Routing;
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Http;
 
-namespace qckdev.Net.Http.Test.InnerService
-{
-    static class Program
-    {
+//namespace qckdev.Net.Http.Test.InnerService
+//{
+//    static class Program
+//    {
 
-        public static void Run(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+//        public static void Run(string[] args)
+//        {
+//            CreateHostBuilder(args).Build().Run();
+//        }
 
-        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .Configure(app =>
-                {
-                    app.UseRouter(endpoints =>
-                    {
-                        endpoints.MapGet("/", async context =>
-                        {
-                            await context.Response.WriteAsync("Hello World!");
-                        });
-                    });
-                });
-    }
-}
+//        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
+//            WebHost.CreateDefaultBuilder(args)
+//                .Configure(app =>
+//                {
+//                    app.UseRouter(endpoints =>
+//                    {
+//                        endpoints.MapGet("/", async context =>
+//                        {
+//                            await context.Response.WriteAsync("Hello World!");
+//                        });
+//                    });
+//                });
+//    }
+//}
 #else
 #endif
